0aa2d11245be64daa7d0684fd84fcce1f6987302
David Churn <dchurn@me.com>
2019-03-21 18:28:04 -0500

    resolved select issues

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');  // JSON parser
 const cors = require('cors');  // security
 const express = require('express');  // handles server events
 const {sequelize} = require ('./database/connection');
+const {Op} = require('./database/connection');
 const {Game} = require('./models/games');
 const {Play} = require('./models/plays');
 
@@ -18,15 +19,9 @@ const app = express();
 app.use(cors({corsOptions}));
 app.use(bodyParser.json());
 
-
-// what port should I listen for queries?
-app.listen(3000, () => {
-  console.log(`>>>Gamer server started<<<`);
-})
-
 //===>  Games Code
 // get the Game information
-app.get('/games', cors(corsOptions), (req,res) => {
+app.get('/games', (req,res) => {
   Game.findAll ({
   })
   .then (games => {
@@ -37,10 +32,29 @@ app.get('/games', cors(corsOptions), (req,res) => {
   })
 })
 
-//  get one game
-app.get('/game/:id', cors(corsOptions), (req,res) => {
+//  get named game ignoring edition
+app.get('/game/name/:name', (req,res) => {
+  Game.findAll({
+    where : { nameTx: { [Op.like]: decodeURIComponent(req.params.name) + '%' }}
+  })
+  .then (game => {
+    res.send(game);
+  })
+  .catch ((error) => {
+    res.send(error);
+  })
+
+})//  get named game with edition
+app.get('/game/name/:name/edition/:edition', (req,res) => {
+  let editionStr = decodeURIComponent(req.params.edition);
+  if (req.params.edition === '') {
+    editionStr = null;
+  }
   Game.findOne({
-    where : { gameID : req.params.id }
+    where : {
+      nameTx : decodeURIComponent(req.params.name),
+      editionTx: editionStr
+      }
   })
   .then (game => {
     res.send(game);
@@ -51,7 +65,7 @@ app.get('/game/:id', cors(corsOptions), (req,res) => {
 })
 
 // Add a new game
-app.post ('/addgame', cors(corsOptions), (req,res) => {
+app.post ('/addgame', (req,res) => {
   Game.create({
     nameTx : req.body.nameTx,
     editionTx : req.body.editionTx,
@@ -79,7 +93,7 @@ app.post ('/addgame', cors(corsOptions), (req,res) => {
 })
 
 // Update all the columns for one game
-app.put ('/chggame/:id', cors(corsOptions), (req,res) => {
+app.put ('/chggame/:id', (req,res) => {
   Game.update({
     nameTx : req.body.nameTx,
     editionTx : req.body.editionTx,
@@ -111,7 +125,7 @@ app.put ('/chggame/:id', cors(corsOptions), (req,res) => {
 })
 
 //  Delete one game
-app.delete ('/delgame/:id', cors(corsOptions), (req,res) => {
+app.delete ('/delgame/:id', (req,res) => {
   Game.destroy({
     where: {
       gameID : req.params.id
@@ -127,7 +141,7 @@ app.delete ('/delgame/:id', cors(corsOptions), (req,res) => {
 
 //===>  plays Code
 // get a list of the play lines.
-app.get('/plays', cors(corsOptions), (req,res) => {
+app.get('/plays', (req,res) => {
   Play.findAll({
   })
   .then (plays => {
@@ -138,7 +152,7 @@ app.get('/plays', cors(corsOptions), (req,res) => {
   })
 })
 
-app.post ('/addplay', cors(corsOptions), (req,res) => {
+app.post ('/addplay', (req,res) => {
   Play.create({
     nameTx : req.body.nameTx,
     editionTx : req.body.editionTx,
@@ -156,7 +170,7 @@ app.post ('/addplay', cors(corsOptions), (req,res) => {
   })
 })
 // Update all the columns for one play
-app.put ('/chgplay/:id', cors(corsOptions), (req,res) => {
+app.put ('/chgplay/:id', (req,res) => {
   Play.update({
     nameTx : req.body.nameTx,
     editionTx : req.body.editionTx,
@@ -179,7 +193,7 @@ app.put ('/chgplay/:id', cors(corsOptions), (req,res) => {
 })
 
 //  Delete one play
-app.delete ('/delplay/:id', cors(corsOptions), (req,res) => {
+app.delete ('/delplay/:id', (req,res) => {
   console.log(req.params);
   Play.destroy({
     where: {
@@ -192,4 +206,10 @@ app.delete ('/delplay/:id', cors(corsOptions), (req,res) => {
   .catch ((error) => {
     res.send(error);
   })
+})
+
+
+// what port should I listen for queries?
+app.listen(3000, () => {
+  console.log(`>>>Gamer server started<<<`);
 })
